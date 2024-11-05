00691c68bfe7fa8604f3133335955d82d2cd19d6
isabelboscan <isabelboscan9@gmail.com>
2024-11-05 03:06:04 +0000

    arreglos 2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -24,7 +24,7 @@ const TrafficLight = () => {
 			<div className="bg-black mx-auto" style={{ width: "30px", height: "80px" }}></div>
 			<div className="bg-black mx-auto rounded" style={{ width: "120px", height: "300px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around" }}>
 				<button onClick={() => handleClick('red')} style={getButtonStyle('red')}></button>
-				<button onClick={() => handleClick('orange')} style={getButtonStyle('orange')}></button>
+				<button onClick={() => handleClick('yellow')} style={getButtonStyle('yellow')}></button>
 				<button onClick={() => handleClick('green')} style={getButtonStyle('green')}></button>
 			</div>
 		</div>
