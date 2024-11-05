221caa8c9ec570f32368bf333b70524a37edcfcc
isabelboscan <isabelboscan9@gmail.com>
2024-11-05 03:04:30 +0000

    arreglos y cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,34 @@
-import React from "react";
-import TrafficLights from "./Lights";
+import React, { useState } from "react";
 
-//include images into your bundle
+const TrafficLight = () => {
+	const [activeColor, setActiveColor] = useState(null);
 
-//create your first component
-const Home = () => {
-	return (	
-		<>
-		<TrafficLights/>
-		</>
-	)
+	const handleClick = (color) => {
+		setActiveColor(color);
+	};
+
+	const getButtonStyle = (color) => {
+		return {
+			width: "80px", 
+			height: "80px", 
+			backgroundColor: color,
+			boxShadow: activeColor === color ? "0 0 20px rgba(255, 255, 255, 1)" : "none",
+			border: "none",
+			borderRadius: "50%",
+			cursor: "pointer"
+		};
+	};
+
+	return (
+		<div className="container">
+			<div className="bg-black mx-auto" style={{ width: "30px", height: "80px" }}></div>
+			<div className="bg-black mx-auto rounded" style={{ width: "120px", height: "300px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around" }}>
+				<button onClick={() => handleClick('red')} style={getButtonStyle('red')}></button>
+				<button onClick={() => handleClick('orange')} style={getButtonStyle('orange')}></button>
+				<button onClick={() => handleClick('green')} style={getButtonStyle('green')}></button>
+			</div>
+		</div>
+	);
 };
 
-export default Home;
+export default TrafficLight;
